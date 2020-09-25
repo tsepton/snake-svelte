f75398824d473232d4c495ecf14a963209d30f34
tsepton <tsepton@gmail.com>
2020-09-25 17:40:37 +0200

    Add verification over user input

--- src/Components/Settings.svelte
+++ src/Components/Settings.svelte
@@ -1,5 +1,25 @@
 <script>
   export let snake;
+
+  $: {
+    if (snake.gameboard.size > 15) {
+      snake.gameboard.size = 15
+    } else if (snake.gameboard.size < 5) {
+      snake.gameboard.size = 5
+    }
+
+    if (snake.gameboard.candySpawn > 100) {
+      snake.gameboard.candySpawn = 100
+    } else if (snake.gameboard.candySpawn < 1) {
+      snake.gameboard.candySpawn = 1
+    }
+
+    if (snake.speed > 10) {
+      snake.speed = 10
+    } else if (snake.speed < 1) {
+      snake.speed = 1
+    }
+  }
 </script>
 
 <style>
@@ -10,6 +30,10 @@
   }
 
   h2 {
+    font-weight: 200;
+  }
+
+  p {
     font-weight: 100;
   }
 </style>
@@ -17,12 +41,16 @@
 <div id="settings">
   <h2>Game Settings</h2>
   <div>
-    <p>Size (5 - 15) :</p><input type="number" bind:value={snake.gameboard.size} />
+    <p>Size (5 - 15) :</p><input
+      type="number"
+      bind:value={snake.gameboard.size} />
   </div>
   <div>
-    <p>Speed (1 to 5) :</p><input type="number" bind:value={snake.speed}/>
+    <p>Speed (1 - 10) :</p><input type="number" bind:value={snake.speed} />
   </div>
   <div>
-    <p>Candy spawn (1 - 100) :</p><input type="number" bind:value={snake.gameboard.candySpawn} />
+    <p>Candy spawn (1 - 100) :</p><input
+      type="number"
+      bind:value={snake.gameboard.candySpawn} />
   </div>
 </div>
